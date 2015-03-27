8d90daf849342cdb76d7213d1184f108c9402aa4
MonsieurW <adressedallan@gmail.com>
2015-03-27 20:49:33 +0100

    version initiale

--- interaction.js
+++ interaction.js
@@ -1,7 +1,7 @@
 //ouverture sous parties pages items pedas et classes
 $('body').on('click','ul li', function(){
 //petit coup d'ajax pour générer sous parties
-$(this).next('ul').slideToggle();
+$(this).toggleClass('actif').next('ul').slideToggle();
 });
 
 
@@ -9,8 +9,8 @@ $(this).next('ul').slideToggle();
 //Génération du pop-up: choix évaluation
 $('#evalchx').on('click', function(){
 	var liste="";
-		for(var i in evals){
-			liste+='<h6 data-ideval="'+i+'">'+evals[i]['nom']+'</h6>';
+		for(var i in evals2){
+			liste+='<h6 data-ideval="'+i+'">'+evals2[i][0]+'</h6>';
 		}
 	genererpopup("eval","CHOIX DE L'EVALUATION",'evalchx">',liste);
 });
@@ -27,10 +27,10 @@ $('#popup').on('click','.evalchx h6', function(){
 
 
 //Génération du pop-up: choix classe
-$('.classe').on('click', function(){
+$('#classe').on('click', function(){
 	liste="";
-		for(var i in classes){
-			liste+='<h6 data-idclasse="'+i+'">'+classes[i]['nom']+'</h6>';
+		for(var i in ttes_classes){
+			liste+='<h6 data-idclasse="'+i+'">'+ttes_classes[i]+'</h6>';
 		}
 	genererpopup("classe","CHOIX DE LA CLASSE",'classechx">',liste);
 });
@@ -38,58 +38,12 @@ $('.classe').on('click', function(){
 $('#popup').on('click','.classechx h6', function(){
 	IDclasse=$(this).data('idclasse');
 	
-	$('#titre .classe').html($(this).html());
+	$('#classe').html($(this).html());
 	$('#cases').empty();
 	if($('#corps').hasClass('evaluer')){gen_case_eleves(IDclasse);}
 	
 //changer l'information évaluation remplie ou pas	
 	$('#popup').slideToggle();
-});
-
-input={2:'A',1:'B',0:'C'};
-
-//générer pop-up pour évaluer un élève
-$('#cases').on('click','article', function(){
-
-	IDeleve=parseInt($(this).data('idelv'));
-	results[IDeleve]={};
-	nom_elv=$(this).children('.nom').html();
-	
-	var liste="";
-	e=evals[IDeval];
-//remettre résultat précédement entré
-	var c=Object.keys(e).length;
-		for(i=1;i<c;i++){taxo=e[i][2];
-			liste+='<article data-idque="'+e[i][0]+'"><aside><h5>'+i+')'+e[i][1]+'</h5><div><span class="awsm">'+taxos[taxo][1]+'</span> '+taxos[taxo][0]+'<span class="note">/'+e[i][3]+'</span></div><div><span class="awsm">&#xf19d;</span> '+e[i][4]+'</div><div><span class="awsm">&#xf085;</span> '+e[i][5]+'</div></aside><div class="input">';
-			idq=parseInt(e[i][0]);
-			//console.log(IDeleve);console.log(idq);
-			//console.log(results[IDeleve][idq]); <- Indéfini, bordel!
-			//results[3202][1234];console.log(results[3202][1234])
-			for(key in input){
-				liste+='<div data-val="'+key+'"';
-				if(results[IDeleve][idq]==parseInt(key)){
-					liste+=' class="actif"';
-				}	
-				liste+='>'+input[key]+'</div>';	
-			}	
-			liste+='</article>';
-		}
-	genererpopup("eval","EVALUER "+nom_elv+" SUR "+e['nom'],'inputresult">',liste);
-	
-});
-
-$('#popup').on('click','.input div', function(){
-	$(this).siblings().removeClass('actif');
-	$(this).addClass('actif');
-	var val=parseInt($(this).data('val'));
-	IDque=parseInt($(this).parents('article').data('idque'));
-	results[IDeleve][IDque]=val;
-	
-	//console.log(results);
-
-//Exemple de tableau de résultats
-//results={IDeleve:{IDquestion1:valeur,IDquestion2:valeur}};
-
 });
 
 
--- maestria.js
+++ maestria.js
@@ -1,47 +1,61 @@
-///////////////donn√©es toutes pages/////////////////////////////
-//Valeur par d√©faut
+
+///////////////donnÈes toutes pages/////////////////////////////
+//Valeurs par dÈfaut
 IDeval=111;
 IDclasse=32;
 results={};
 
-evals={111:{
-		"nom":'Puissance',//ID,titre,,note,niveau taxo,conn,comp
-		1:[1234,"D√©finition puissance",2,2,"la puissance est une √©nergie par unit√© de temps","Savoir s'exprimer √† l'aide du langage scientifique"],
-		2:[1235,"Calcul Puissance",3,3,"la puissance est une √©nergie par unit√© de temps","Savoir utiliser une calculatrice"],
-		3:[1236,"Diagramme √©nerg√©tique",3,5,"L'√©nergie se transforme","Sch√©matiser selon les conventions"]},
-	222:{
-		"nom":'D√©coupage',
-		1:[9876,"Nommer les ciseaux",2,1,"Savoir qu'on parle d'une paire de ciseaux","Savoir nommer les objets de la classe"],
-		2:[9877,"Placement main",3,2,"","Mettre le pouce vers la lumi√®re quand on d√©coupe"],
-		3:[9878,"Adresse courbe",3,2,"","Savoir d√©couper une courbe avec des ciseaux"],
-		4:[9879,"Adresse droite",3,2,"","Savoir d√©couper une ligne droite avec des ciseaux"],
-		5:[9880,"Adresse",3,1,"","Savoir comment reconna√Ætre les ciseaux gauchers"],
-		6:[9881,"Respect",4,3,"","Ne pas planter les ciseaux dans ses camarades"],
-		7:[9882,"Capillotract√©",3,4,"","Savoir d√©couper une frange avec des ciseaux"]
-		}
-	};
-	
+//Sert ‡ chaque fois qu'on peut changer de classe
+ttes_classes={31:'3<sup>1</sup>',32:'3<sup>2</sup>',33:'3<sup>3</sup>'};
+//Sert ‡ chaque fois qu'on peut changer d'Èvaluation
+evals2={111:['Puissance','07/03/2015'],222:['DÈcoupage','14/03/2015']};
+//Sert pour la page correction et	index
 taxos=["",
-	["Conna√Ætre","&#xf02d;"],
+	["ConnaÓtre","&#xf02d;"],
 	["Comprendre","&#xf0e2;"],
 	["Appliquer","&#xf0ad;"],
 	["Analyser","&#xf005;"]];	
 	
-
+	
+//sert pour la page neweval (en mode modification) et la page index	
+	evals={111:{
+		"nom":'Puissance',
+		"date":'1986-03-14',
+		//ID,titre,,note,niveau taxo,conn,comp
+		1:[1234,"DÈfinition puissance",2,2,"la puissance est une Ènergie par unitÈ de temps","Savoir s'exprimer ‡ l'aide du langage scientifique"],
+		2:[1235,"Calcul Puissance",3,3,"la puissance est une Ènergie par unitÈ de temps","Savoir utiliser une calculatrice"],
+		3:[1236,"Diagramme ÈnergÈtique",3,5,"L'Ènergie se transforme","SchÈmatiser selon les conventions"]},
+	222:{
+		"nom":'DÈcoupage',
+		"date":'1984-04-20',
+		1:[9876,"Nommer les ciseaux",2,1,"Savoir qu'on parle d'une paire de ciseaux","Savoir nommer les objets de la classe"],
+		2:[9877,"Placement main",3,2,"","Mettre le pouce vers la lumiËre quand on dÈcoupe"],
+		3:[9878,"Adresse courbe",3,2,"","Savoir dÈcouper une courbe avec des ciseaux"],
+		4:[9879,"Adresse droite",3,2,"","Savoir dÈcouper une ligne droite avec des ciseaux"],
+		5:[9880,"Adresse",3,1,"","Savoir comment reconnaÓtre les ciseaux gauchers"],
+		6:[9881,"Respect",4,3,"","Ne pas planter les ciseaux dans ses camarades"],
+		7:[9882,"CapillotractÈ",3,4,"","Savoir dÈcouper une frange avec des ciseaux"]
+		}
+	};
+	
+//sert pour la page index et classe (certaines infos ne sont pas nÈcessaires pour la page classe)
 classes={
 	31:{                                                             //ID classe
 			'nom':'3<sup>1</sup>',
-			1:[3101,'Allan Wauters de Besterfeld',80,90,30,95,20,50],//position:[ID,nom,taxo1,taxo2,taxo3,taxo4,pourcentage couleur,pourcentage chiffre
+			'optaff':[6,'3-2','3-2-2'],//rangÈe,chiffre,couleur
+			//position:[ID,nom,taxo1,taxo2,taxo3 ,taxo4,pourcentage couleur,pourcentage chiffre
+			1:[3101,'Allan Wauters de Besterfeld',80,90,30,95,20,50],
 			2:[3102,'Nana Cerise',10,20,80,100,30,69],
 			3:[3103,'Nicolas',90,100,40,100,30,60]		
 		},
 	32:{
 		'nom':'3<sup>2</sup>',
+		'optaff':[8,'1','2'],
 		1:[3201,'Nicolass',90,100,40,100,30,60],
 		2:[3202,'Allann Wauters de Besterfeld',80,90,30,95,20,50],
 		3:[3203,'Nanana Cerise',90,100,40,100,30,60],
-		4:[3204,'Bertrand',90,100,40,100,30,60],
-		5:[3205,'Aliz√©',80,90,30,95,20,50],
+		4:[3204,'Bertrand',10,20,30,40,30,60],
+		5:[3205,'AlizÈ',50,60,70,80,20,50],
 		6:[3206,'Massilia',90,100,40,100,30,60],
 		7:[3207,'Ernest',90,100,40,100,30,60],
 		8:[3209,'Biroute',80,90,30,95,20,50],
@@ -53,38 +67,41 @@ classes={
 		14:[3215,'Berthe aux grands pieds',80,90,30,95,20,50],
 		15:[3216,'Bruno',90,100,40,100,30,60],
 		16:[3217,'Phillipe',2,3,4,5,8,10],
-		17:[3218,'Kuntakint√©',80,90,30,95,20,50],
+		17:[3218,'KuntakintÈ',80,90,30,95,20,50],
 		18:[3219,'Zoubida',90,100,40,100,30,60]	
 		},
 	33:{
 		'nom':'3<sup>3</sup>',
+		'optaff':[2,'3','3-1'],
 		1:[331,'Mohammed',90,100,40,100,30,60],
 		2:[332,'Bachibouzouk',80,90,30,95,20,50],
-		3:[333,'Cun√©gonde',90,100,40,100,30,60]}
+		3:[333,'CunÈgonde',90,100,40,100,30,60]}
 };	
-//Chaque domaine a un ID,on rajoute deux chiffres √† cet ID pr avoir celui d'un theme fils et encore 2 pr un item
+//Chaque domaine a un ID de deux chiffre,on rajoute deux chiffres ‡ cet ID pr avoir celui d'un theme fils et encore 1 pr un item
 items={
-	1:['Physique'],
-	2:['Chimie'],
-	3:['Savoir-faire',{01:'R√©aliser Test'},
-					{02:"Interpreter test","items":["Associer les grandeurs aux unit√©s correspondantes","Verifier la coh√©rence des r√©sultats obtenus","Avoir l'esprit critique vis-√†-vis de ses r√©sultats","Utiliser du vocabulaire de la m√©trologie","Percevoir la diff√©rence entre r√©alit√© et simulation"]},
-					{03:"D√©marche d√©ductive"}
-	
-	],
-	4:["Attitude"]};
+	1:{"nom":'Physique'},
+	2:{"nom":'Chimie',
+			01:["Corps pur",["Un corps pur est constituÈ d'un seul type de molÈcule"]]
+	},
+	3:{"nom":'Savoir-faire',
+		01:['RÈaliser Test'],
+		02:["Interpreter test",
+			["Associer les grandeurs aux unitÈs correspondantes","Verifier la cohÈrence des rÈsultats obtenus","Avoir l'esprit critique vis-‡-vis de ses rÈsultats","Utiliser du vocabulaire de la mÈtrologie","Percevoir la diffÈrence entre rÈalitÈ et simulation"]
+			],
+		03:["DÈmarche dÈductive"]
+	},
+	4:{"nom":"Attitude"}};
 
-ic_list='<span class="awsm del">&#xf1f8;</span><span class="awsm edit">&#xf040;</span>';
+
+
 
 	
-	
-	
-	
-//////////////////////////////G√©n√©ration pop up
+//////////////////////////////GÈnÈration pop up
 fixepopup =['<section class="titre ','"><div class="awsm exit">&#xf00d;</div><h3>','</h3></section><section class="contenu ','</section>'];
 
 function genererpopup(clas,titr,clas2,contenu){
 	$('#inpopup').html(fixepopup[0]+ clas +fixepopup[1]+ titr +fixepopup[2]+clas2+contenu+fixepopup[3]);	
-	$('#popup').slideToggle();
+	$('#popup').slideDown();
 }	
 	
 	
@@ -103,66 +120,71 @@ function couleur(y){
 	return rgetvert	;
 }
 
+function finditem(id){
+console.log(id);
+	if(id!=""){
+		split=id.split('-');}
+	else{return '';}
+	console.log(split[0]+'/'+split[1]+'/'+split[2]);
+	if(split[2]!=undefined){
+		return items[split[0]][split[1]][1][split[2]];
+	}
+	else{
+		if(split[1]==undefined){
+		return items[split[0]]['nom'];
+		}
+		return items[split[0]][split[1]][0];
+	}
+}
 
 /////////////////Correction
 
-//Items utilis√© lors de l'√©valuation
-savoirs=["Le feu √ßa brule","L'eau, √ßa mouille","Tous les oiseaux volent dans le ciel","Ta soeur bat le beurre √† 300 √† l'heure sur le cyclo du facteur!"];
-sf_att=["Utiliser une fourchette","Savoir l√©cher son coude","Pouvoir √©crire son nom dans la neige","Savoir quand se taire et quand parler √† tr√®s bon escient ;-)"];
-//cette liste ne devra pas comporter les √©l√®ves n'ayant pas √©t√© √©valu√©s
-//nom,note,graphe connaitre,note conna√Æte, graphe comprendre,note,graphe appliquer, note, graphe analyse, note
-correc=[
-	['Nicolas',2,'graphe1','graphe2','graphe3','graphe4',20,20,20,20,'l','l','l','l','l','l','l','l'],
-	['Allann Wauters de Besterfeld',18,'graphe1','graphe2','graphe3','graphe4',50,70,50,90,'l','l','l','l','l','l','l','l'],
-	['Nanana Cerise',18,'graphe1','graphe2','graphe3','graphe4',90,50,80,40,'l','l','l','l','l','l','l','l'],
-	['Bertrand',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Massilia',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Ernest',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Biroute',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Julien',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Maxime',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Laurent',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Catherine',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Le Suisse',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Berthe aux grands pieds',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Bruno',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Philippe',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Kuntakint√©',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l'],
-	['Zoubida',18,'graphe1','graphe2','graphe3','graphe4',80,70,60,90,'l','l','l','l','l','l','l','l']];
-	
 
-//g√©n√©rer la liste des items en lui donnant la balise de destination
-function list_item(destination,add){
-list='';//var c=Object.keys(items).length	//console.log(values(items[3][1]))
+	
+ic_list1='<span class="awsm del">&#xf1f8;</span><span class="awsm edit">&#xf040;</span>';
+
+
+//gÈnÈrer la liste des items en lui donnant la balise de destination
+function list_item(destination,add,ic_list,itemprio){
+console.log(itemprio);
+if(itemprio!=false){
+split=itemprio.toString().split('-');}
+else{split=['','',''];}
+list='';
+	//console.log(values(items[3][1]))
 for(var i in items){
-	list+='<li data-id="'+i+'">'+items[i][0]+ic_list+'</li>';
-	var c=items[i].length;
-	if(c>1){list+='<ul>';
-		for(var j=1;j<c;j++){
-			for(var k in items[i][j]){
-				if(k=='items'){
-					list+='<ul>';
-					itemm=items[i][j][k];
-					var d=itemm.length;
-					for(var l=0;l<c;l++){
-						list+='<li data-id="'+idtheme+l+'">'+itemm[l]+ic_list+'</li>';
+	list+='<li data-id="'+i+'"';
+	list+=(split[0]==i)?' class="actif"' :'';
+	list+='>'+items[i]['nom']+ic_list+'</li>';
+	var c=Object.keys(items[i]).length;
+	if(c>1){
+		list+=(split[0]==i)?'<ul style="display:block">' :'<ul>';
+		for(var j in items[i]){
+			if(j!="nom"){
+				list+='<li data-id="'+i+'-'+j+'"';
+						list+=(split[1]==j)?' class="actif"' :'';
+						list+='>'+items[i][j][0]+ic_list+'</li>';
+				d=items[i][j][1];
+				if(d!=undefined){
+					list+=(split[1]==j)?'<ul style="display:block">' :'<ul>';
+					dl=d.length;
+					for(var k=0;k<dl;k++){
+						list+='<li data-id="'+i+'-'+j+'-'+k+'"';
+						list+=(parseInt(split[2])===k)?' class="actif"' :'';
+						list+='>'+d[k]+ic_list+'</li>';
 					}
 					if(add){list+='<span class="awsm add" title="Ajouter item">&#xf0fe;</span>';}
-					list+='</ul>';	
+					list+='</ul>';
 				}
-				else{
-				idtheme=i+k;
-				list+='<li data-id="'+idtheme+'">'+items[i][j][k]+ic_list+'</li>';
-				}
+				
 			}
-			
 		}
-		if(add){list+='<span class="awsm add" title="Ajouter th√®me">&#xf0fe;</span>';}
+		if(add){
+		list+='<span class="awsm add" title="Ajouter thËme">&#xf0fe;</span>';}
 		list+='</ul>';
-	}
-			
-}
-if(add){list+='<span class="awsm add" title="Ajouter domaine">&#xf0fe;</span>';}	
+	}	
+}			
+if(add){list+='<span class="awsm add" title="Ajouter domaine">&#xf0fe;</span>';}
 $(destination).html(list); 
 }
 
